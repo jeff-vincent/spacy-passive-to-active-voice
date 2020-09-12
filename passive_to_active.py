2e75ca22a8a57dc1225f7194093036245b93ea32
Ubuntu <ubuntu@ip-172-31-0-137.us-west-2.compute.internal>
2020-09-12 07:42:30 +0000

    Init

---
+++ passive_to_active.py
@@ -0,0 +1,61 @@
+import spacy
+
+pronouns = {'her':'she', 'him':'he', 'whom':'who', 'me': 'I', 'us':'we', 'them':'they'}
+
+nlp = spacy.load('en_core_web_sm')
+
+with open('passive.txt', 'r') as text:
+    doc = nlp(text.read())
+
+sents = list(doc.sents)
+
+for sent in doc.sents:
+    try:
+        sent_list = []
+        for token in sent:
+            sent_list.append(token)
+
+        main_verb_index = None
+        agent_index = None
+        recipient_index = None
+        agent = ''
+        append = False
+    
+        index_counter = 0
+
+        for token in sent_list:
+            if token.dep_ in ['nsubjpass', 'nsubj']:
+                recipient_index = index_counter
+                recipient = token.lower_
+            if token.dep_ == 'ROOT':
+                main_verb_index == index_counter
+                if token.tag_ == 'PP':
+                    print('Need to convert {} to simple past.'.format(token.text))
+                main_verb = token.text
+            if token.dep_ == 'agent':
+                agent_index = index_counter
+                agent = token.text
+                append = True
+            if agent and token.text != agent and not token.is_punct and agent != '*':
+                agent = agent + ' ' + token.text
+            if token.is_punct:
+                punct = token.text
+            if token.dep_ == 'pobj':
+               append = False
+               if not agent:
+                   agent = token.text
+
+            index_counter += 1
+
+        for key, value in pronouns.items():
+            if key in agent.lower():
+                agent = value
+
+        if recipient_index < agent_index:
+            print('-'*50+'\n {}'.format(
+                sent)+'\n Active voice skeleton: {} {} the {}{}'.format(
+                    agent, main_verb, recipient, punct).replace('by the', 'The').replace('by', ''))
+    except:
+        print(sent)
+        for token in sent:
+            print('Text: ' + token.text + ' |   Dep: ' + token.dep_ + ' |   Tag: ' + token.tag_)
