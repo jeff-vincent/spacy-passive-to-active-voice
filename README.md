05f037d15ce8e72954d8920fa5a8f9073803ba20
Ubuntu <ubuntu@ip-172-31-0-137.us-west-2.compute.internal>
2020-09-12 23:58:29 +0000

    Improve and add README

---
+++ README.md
@@ -0,0 +1,71 @@
+# Example Output
+
+```
+--------------------------------------------------
+ At dinner, six shrimp were eaten by Harry.
+
+ Active voice skeleton: Harry  
+ eaten six shrimp.
+--------------------------------------------------
+ The savannah is roamed by beautiful giraffes.
+
+ Active voice skeleton: beautiful  giraffes  
+ roamed the savannah.
+--------------------------------------------------
+ The flat tire was changed by Sue.
+
+ Active voice skeleton: Sue  
+ changed the flat tire.
+--------------------------------------------------
+ A movie is going to be watched by us tonight.
+
+ Active voice skeleton: we going a movie.
+--------------------------------------------------
+ The obstacle course was run by me in record time.
+
+ Active voice skeleton: I run the obstacle course.
+--------------------------------------------------
+ The entire stretch of highway was paved by the crew.
+
+ Active voice skeleton: The  crew  
+ paved the entire stretch.
+--------------------------------------------------
+ The novel was read by Mom in one day.
+
+ Active voice skeleton: Mom  in  one  day  
+ read the novel.
+--------------------------------------------------
+ The comet was viewed by the science class.
+
+ Active voice skeleton: The  science  class  
+ viewed the comet.
+--------------------------------------------------
+ The video was posted on Facebook by Alex.
+
+ Active voice skeleton: Alex  
+ posted the video.
+--------------------------------------------------
+ Instructions will be given to you by the director.
+
+ Active voice skeleton: The  director  
+  will given  instructions.
+--------------------------------------------------
+ The Grand Canyon is viewed by thousands of tourists every year.
+
+ Active voice skeleton: thousands  of  tourists  every  year  
+ viewed the grand canyon.
+--------------------------------------------------
+ The victory will be celebrated by the team tomorrow.
+
+ Active voice skeleton: The  team  tomorrow  
+  will celebrated the victory.
+--------------------------------------------------
+ The metal beams were eventually corroded by the saltwater.
+
+ Active voice skeleton: The  saltwater  
+ corroded the metal beams.
+--------------------------------------------------
+ The baby was carried by the kangaroo in her pouch.
+
+ Active voice skeleton: she carried the baby.
+```
--- passive_to_active.py
+++ passive_to_active.py
@@ -1,13 +1,11 @@
 import spacy
 
-pronouns = {'her':'she', 'him':'he', 'whom':'who', 'me': 'I', 'us':'we', 'them':'they'}
+pronouns = {'her ':'she ', 'him ':'he ', 'whom ':'who ', 'me ': 'I ', 'us ':'we ', 'them ':'they '}
 
 nlp = spacy.load('en_core_web_sm')
 
 with open('passive.txt', 'r') as text:
     doc = nlp(text.read())
-
-sents = list(doc.sents)
 
 for sent in doc.sents:
     try:
@@ -20,6 +18,7 @@ for sent in doc.sents:
         recipient_index = None
         agent = ''
         append = False
+        inflection = ''
     
         index_counter = 0
 
@@ -27,35 +26,42 @@ for sent in doc.sents:
             if token.dep_ in ['nsubjpass', 'nsubj']:
                 recipient_index = index_counter
                 recipient = token.lower_
+                article_list = [t.text for t in token.lefts]
+                try:
+                    compound_article = ' '.join(article_list).lower()
+                except:
+                    compound_article = ''
             if token.dep_ == 'ROOT':
                 main_verb_index == index_counter
+                main_verb = token.text
                 # TODO: convert VBN to VBD
                 # if token.tag_ == 'VBN':
-                
-                main_verb = token.text
+            
             if token.dep_ == 'agent':
                 agent_index = index_counter
                 agent = token.text
                 append = True
             if agent and token.text != agent and not token.is_punct:
-                agent = agent + ' ' + token.text
+                agent = agent + ' ' + token.text + ' '
             if token.is_punct:
                 punct = token.text
             if token.dep_ == 'pobj':
                append = False
                if not agent:
                    agent = token.text
+            if token.text in ['may', 'must', 'might', 'could', 'will']:
+                inflection = ' ' + token.text + ' '
 
             index_counter += 1
 
         for key, value in pronouns.items():
-            if key in agent.lower():
+            if key in agent.lower() + ' ':
                 agent = value
 
         if recipient_index < agent_index:
             print('-'*50+'\n {}'.format(
-                sent)+'\n Active voice skeleton: {} {} the {}{}'.format(
-                    agent, main_verb, recipient, punct).replace('by the', 'The').replace('by', ''))
+                sent)+'\n Active voice skeleton: {}{}{} {} {}{}'.format(
+                    agent, inflection, main_verb, compound_article, recipient, punct).replace('by the', 'The').replace(' by ', ' '))
     except:
         print(sent)
         for token in sent:
