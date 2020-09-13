765cf52a3571763fbac9534efa9eb2f9ad32ef62
Ubuntu <ubuntu@ip-172-31-0-137.us-west-2.compute.internal>
2020-09-13 10:35:08 +0000

    Expand to consider token children

--- passive_to_active.py
+++ passive_to_active.py
@@ -19,6 +19,9 @@ for sent in doc.sents:
         agent = ''
         append = False
         inflection = ''
+        agent_article = ''
+        start_word = ''
+        sentence_remainder = []
     
         index_counter = 0
 
@@ -34,15 +37,19 @@ for sent in doc.sents:
             if token.dep_ == 'ROOT':
                 main_verb_index == index_counter
                 main_verb = token.text
+                base_form_of_main_verb = token.lemma_
                 # TODO: convert VBN to VBD
                 # if token.tag_ == 'VBN':
             
             if token.dep_ == 'agent':
                 agent_index = index_counter
-                agent = token.text
+                agent_children_list = [t.text for t in token.rights]
+                agent_children_right = ' '.join(agent_children_list)
+                agent = token.text + ' ' + agent_children_right
+                agent_children_left = [t.text for t in token.lefts]
                 append = True
-            if agent and token.text != agent and not token.is_punct:
-                agent = agent + ' ' + token.text + ' '
+            if agent and not token.is_punct:
+                sentence_remainder.append(token)
             if token.is_punct:
                 punct = token.text
             if token.dep_ == 'pobj':
@@ -53,15 +60,27 @@ for sent in doc.sents:
                 inflection = ' ' + token.text + ' '
 
             index_counter += 1
+        if punct in ['?']:
+            for token in sent:
+                if token.text in ['Can', 'May']:
+                    start_word = token.text
+                    break
 
         for key, value in pronouns.items():
             if key in agent.lower() + ' ':
-                agent = value
+                agent = value.strip()
+        index = 0 
+        for t in sentence_remainder:
+            if t.pos_ in ['DET'] and index >= 2:
+                agent_article = t.text
+                index += 1
 
         if recipient_index < agent_index:
             print('-'*50+'\n {}'.format(
-                sent)+'\n Active voice skeleton: {}{}{} {} {}{}'.format(
-                    agent, inflection, main_verb, compound_article, recipient, punct).replace('by the', 'The').replace(' by ', ' '))
+                sent)+'\n Active voice skeleton:{} {} {} {}{} {} {}{}'.format(
+                    start_word, agent_article, agent, inflection, main_verb, compound_article, recipient, punct).replace(
+                        'by ', ''))
+            print('Base form of main verb: {}'.format(base_form_of_main_verb))
     except:
         print(sent)
         for token in sent:
